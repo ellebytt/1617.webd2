798d123429318d9f2ea35499f1482e75f4361f31
ellebytt <ellebytt@student.arteveldehs.be>
2017-05-19 14:03:17 +0200

    Versie 19/05

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-;(function() {
-  alert('Welcome to the GDM Web Template developed by drdynscript!');
-})();
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+
+
+
+var map;
+
+function initMap() {
+    var uluru = {lat: 50.839794, lng: 3.274155};
+      map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 50.839794, lng: 3.274155}, 
+          zoom: 10,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
